e24a1df317d6110a414e9e2c194c2e616e83d5b3
wzx971127 <55274387+wzx971127@users.noreply.github.com>
2019-11-23 21:39:57 +0800

    Add files via upload

---
+++ IMGUI.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IMGUI : MonoBehaviour {
+    private void OnGUI()
+    {
+        Vector3 worldPos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        Vector2 screenPos = Camera.main.WorldToScreenPoint(worldPos);
+        GUI.HorizontalScrollbar(new Rect(screenPos.x - 50, screenPos.y, 100, 100), 0, 50, 0, 100);
+    }
+}
